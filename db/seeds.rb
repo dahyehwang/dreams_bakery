70e5c083520ec8dc5280ee461206525f60373d45
Dahye Hwang <dahyechoi88@gmail.com>
2016-08-29 16:41:15 -0700

    add seeds to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+@user1 = User.create([{ first_name: 'Valentyna', last_name: 'Gorbachenko', nickname: 'Valie', email: 'valie@codingdojo.com', password: 'password', password_confirmation: 'password' }])
+@user2 = User.create([{ first_name: 'Sang', last_name: 'Yun', nickname: 'Sang316', email: 'sang316@gmail.com', password: 'password', password_confirmation: 'password' }])
+@user3 = User.create([{ first_name: 'Carlos', last_name:'Chen', nickname: 'Charie', email:'charie86@gmail.com', password: 'password', password_confirmation: 'password' }])
+@user4 = User.create([{ first_name: 'Dahye', last_name: 'Hwang', nickname: 'Grace', email: 'grace@codingdojo.com', password: 'password', password_confirmation: 'password' }])
+
+@parent1 = Parent.create([{}])
+messages = Message.create([{ content: 'Dojo rocks!!!', user: @user1 }])
