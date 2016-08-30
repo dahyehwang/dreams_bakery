35261a49b9df959ab646637e45932ab1989d3069
hccarlos <chenhanchih@gmail.com>
2016-08-29 21:59:59 -0700

    additional content field validation for Message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,15 +3,21 @@ class Message < ActiveRecord::Base
   belongs_to :parent, class_name: "Message"
   
   has_many :likes
-  has_many :users_liked, through: :likes, source: :user
+  has_many :users_liked, through: :likes, source: :user  ## people who liked this message
   has_many :comments, class_name: "Message", foreign_key: "parent_id", dependent: :destroy
 
-  validates :message, presence: true, length: { in: 2..255 }
-  # validates :parent, :word_count_is_less_than_4
+  validates :content, presence: true
+  validates :content, :length => { :maximum => 7, :tokenizer => lambda { |str| str.scan(/\w+/) }, :too_long  => "Please limit your summary to %{count} words" }, unless: :comment?
+  validates :content, length: { in: 2..255 }, unless: :post?
 
   private
 
-	def word_count_is_less_than_4
-    errors[:message] << "too many words" if desc.split.size > 3
+  def comment?
+    parent_id != nil
   end
+
+  def post?
+    parent_id == nil
+  end
+
 end
