a3015fe59b0ae40e0ad9fe642931a875d6aa6426
Dahye Hwang <dahyechoi88@gmail.com>
2016-08-30 21:09:41 -0700

    added create and destroy methods in likes controller plus like and unlike button inshow.html.erb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,18 @@
 class LikesController < ApplicationController
 	def create
-		current_user.likes.create(message: Message.find(params[:message_id]))
-	  redirect_to "messages/#{ params[:message_id] }"
+    unless current_user.likes.where(message: Message.find(params[:message_id])).first
+      current_user.likes.create(message: Message.find(params[:message_id]))
+      redirect_to "/messages/#{ params[:post_id] }"
+    else
+      redirect_to "/messages/#{ params[:post_id] }"
+    end
   end
   def destroy
-  	like = current_user.likes.where(message: Message.find(params[:id]))
-  	like.destroy_all
-  	redirect_to "messages/#{ params[:message_id] }"
+    if current_user.likes.where(message: Message.find(params[:message_id])).first
+      current_user.likes.where(message: Message.find(params[:message_id])).first.destroy
+      redirect_to "/messages/#{ params[:post_id] }"
+    else
+      redirect_to "/messages/#{ params[:post_id] }"
+    end
   end
 end
