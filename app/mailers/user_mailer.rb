3a08310e4843ddb14f8a9c12996c9393417d6b48
hccarlos <chenhanchih@gmail.com>
2016-09-01 23:29:22 -0700

    added correct IP address for app/mailers/user_mailer.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,13 +3,13 @@ class UserMailer < ActionMailer::Base
 
   def welcome_email(user)
   	@user = user
-  	@url = "http://localhost:3000"
+  	@url = "http://52.43.79.159"
   	mail(to: @user.email, subject: 'Welcome to Dreams Bakery!')
   end
   def reply_email(user, parent)
     @parent = parent
   	@user = user
-  	@url = "http://localhost:3000/messages/#{ @parent.id }"
+  	@url = "http://52.43.79.159/messages/#{ @parent.id }"
   	mail(to: @user.email, subject: 'Someone has sent you a reply')
   end
 end
